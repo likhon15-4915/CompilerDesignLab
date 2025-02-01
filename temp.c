fa2d6e29a246f19b2771b853465bf0e9b1091fe8
Md. Raisul Kabir Khan Likhon <likhon15-4915@diu.edu.bd>
2025-02-01 12:40:57 +0600

    Second Commit for prob 1

--- temp.c
+++ temp.c
@@ -1,4 +1,32 @@
-#include<stdio.h>
-int main(){
-printf("Hello World!");
+#include <stdio.h>
+
+int main() {
+int array[100], i, n;
+
+
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+
+
+printf("Enter %d elements: ", n);
+	for(i = 0; i < n; i++) {
+    scanf("%d", &array[i]);
+    }
+
+ 
+printf("Output the same number:\n");
+	for(i = 0; i < n; i++) {
+    printf("%d ", array[i]);
+    }
+
+  
+printf("\nReverse print:\n");
+	for(i = n - 1; i >= 0; i--) {
+        printf("%d ", array[i]);
+    }
+
+return 0;
 }
+
+~
+
